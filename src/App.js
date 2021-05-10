3b38dbc22508a2c7b08a26410c8ffc2f02513e6e
Cin-1 <cinthiapardos@gmail.com>
2021-05-10 15:01:39 -0300

    theme fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css"
 import { ThemeProvider } from "@material-ui/core/styles"
-import theme from "./theme/theme"
+import { theme } from "./theme/theme"
+import SignInSide from "./Components/Login"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <p>Proyecto</p>
+        <SignInSide />
       </div>
     </ThemeProvider>
   )
