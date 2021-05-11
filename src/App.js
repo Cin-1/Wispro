0921f7e1ce1bc5504cb9900314b5b14e21228cba
Cin-1 <cinthiapardos@gmail.com>
2021-05-11 14:24:51 -0300

    user table component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { ThemeProvider } from "@material-ui/core/styles"
 import { theme } from "./theme/theme"
 import SignInSide from "./Components/Login"
-import Orders from "./Components/UsersTable"
+import Users from "./Components/UsersTable"
+import Register from "./Components/Register"
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={SignInSide} exact />
-          <Route path="/users" component={Orders} exact />
+          <Route path="/users" component={Users} exact />
+          <Route path="/register" component={Register} exact />
+
           <Route path="/post/:id" />
         </Switch>
       </BrowserRouter>
