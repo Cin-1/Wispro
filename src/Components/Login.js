bbe1774164c2cdcee4e2801da333d71d3526c394
Cin-1 <cinthiapardos@gmail.com>
2021-05-14 17:32:16 -0300

    login conected redux

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Avatar from "@material-ui/core/Avatar"
 import Button from "@material-ui/core/Button"
 import CssBaseline from "@material-ui/core/CssBaseline"
@@ -11,48 +11,36 @@ import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
 import Swal from "sweetalert2"
 import { connect } from "react-redux"
-import { sessionLogin } from "../Redux/Actions/actionsUsers.js"
+import { LoginUser } from "../Redux/Actions/actionsUsers.js"
+import md5 from "md5"
+import Cookies from "universal-cookie"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh",
-  },
-  image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
-
-function SignInSide() {
+function SignInSide({ LoginUser, userLogged, error, loading }) {
   const classes = useStyles()
-
+  const cookie = new Cookies()
   const [values, setValues] = useState({
     email: "",
     password: "",
   })
+  let history = useHistory()
+
+  const logged = cookie.get("id")
+
+  useEffect(() => {
+    if (userLogged.id) {
+      cookie.set("id", userLogged.id, { path: "/" })
+      cookie.set("name", userLogged.name, { path: "/" })
+      cookie.set("lastname", userLogged.lastName, { path: "/" })
+      cookie.set("email", userLogged.email, { path: "/" })
+      cookie.set("email", userLogged.email, { path: "/" })
+      cookie.set("dni", userLogged.dni, { path: "/" })
+      history.push("/users")
+    }
+  }, [userLogged])
+
+  useEffect(() => {
+    if (logged) history.push("/users")
+  }, [logged])
 
   const handleInputChange = (e) => {
     setValues({
@@ -61,33 +49,12 @@ function SignInSide() {
     })
   }
 
-  let history = useHistory()
-
   const submitForm = (e) => {
     e.preventDefault()
-    sessionLogin(values)
-      .then((res) => {
-        if (res.status === "error") {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Tuviste un Error al iniciar sesion, reingresa tus datos",
-          }).then(() => {
-            setValues({
-              email: "",
-              password: "",
-            })
-          })
-        }
-        if (res.us.active === true) {
-          Swal.fire("Bienvenido").then(() => {
-            history.push("/home")
-          })
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    if (values) {
+      const hashPassword = md5(values.password)
+      LoginUser(values.email, hashPassword)
+    }
   }
 
   return (
@@ -102,6 +69,9 @@ function SignInSide() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
+          {error ? <p>There was an error. Please try again </p> : null}
+          {loading ? <p>Loading...</p> : null}
+
           <form
             className={classes.form}
             noValidate
@@ -149,9 +119,48 @@ function SignInSide() {
     </Grid>
   )
 }
-
-const mapDispatchToProps = (dispatch) => ({
-  sessionLogin: (value) => dispatch(sessionLogin(value)),
+const mapStateToProps = (state) => ({
+  loading: state.loading,
+  userLogged: state.userLogged,
+  error: state.error,
+  redirect: state,
 })
 
-export default connect(null, mapDispatchToProps)(SignInSide)
+const mapDispatchToProps = (dispatch) => ({
+  LoginUser: (email, pass) => dispatch(LoginUser(email, pass)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignInSide)
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
